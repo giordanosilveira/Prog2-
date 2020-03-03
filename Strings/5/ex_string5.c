6d841bd022d0dd8a25baed38340f656aa1f30be2
giordanosilveira <55298486+giordanosilveira@users.noreply.github.com>
2020-03-03 20:40:55 -0300

    Update ex_string5.c

--- Strings/5/ex_string5.c
+++ Strings/5/ex_string5.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 100
-int ehcaracter (char *word, int ind) {
+int ehcaracter (char *word, int ind) { // Basicamente testa se é um caracter válido
     if (word[ind] < '0' || word[ind] > '9') 
         if (word[ind] < 'a' || word[ind] > 'z') 
             if (word[ind] < 'A' || word[ind] > 'Z') 
@@ -19,19 +19,19 @@ int main () {
     char string[MAX + 1];
     int i,j; 
 
-    scanf ("%[^\n]", string);
+    scanf ("%[^\n]", string); // Lê uma string enquando não enxerga o ENTER
     i = 0;
 
-    while (string[i] != '\0') {
+    while (string[i] != '\0') { // Vai até o final da string 
         if (! ehcaracter(string,i)) { 
             j = i;
-            while (string[j+1] != '\0') {
+            while (string[j+1] != '\0') { //Sê não o char n for válido, pega o restante da palavra e puxa-a
                 string[j] = string[j+1];
                     j++;
             }
-            string[j] = string[j+1];
+            string[j] = string[j+1]; // Troca final por causa do \0
         }
-        else
+        else // Se o caracter for válido é que att, senão da pau
             i++;
     }
     printf ("%s\n", string);
