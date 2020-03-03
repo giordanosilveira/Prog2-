7db584457d97fd216147848217556f722868c2a5
giordanosilveira <55298486+giordanosilveira@users.noreply.github.com>
2020-03-03 20:45:58 -0300

    Update ex_string6.c

--- Strings/6/ex_string6.c
+++ Strings/6/ex_string6.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #define MAX 100
 
-int ehcaracter (char *word, int ind) {
+int ehcaracter (char *word, int ind) { // Basicamente chega se o char a frente é igual, se sim, retorna 0
     if (word[ind] == word[ind + 1]) 
         return 0;
     return 1;
@@ -17,23 +17,23 @@ int main () {
     char string[MAX + 1], lixo;
     int i,j; 
 
-    scanf ("%[^\n]", string);
+    scanf ("%[^\n]", string); // Enquanto não enxerga um ENTER, continua lendo
     i = 0;
 
-    while (string[i] != '\0') {
+    while (string[i] != '\0') { // Vai até o final da string
         if (! ehcaracter(string,i)) { 
             j = i;
-            lixo = string[j+1];
-            while (string[j] == lixo) {
-                while (string[j+1] != '\0') {
+            lixo = string[j+1]; // Ajuda na última comparação
+            while (string[j] == lixo) { // Enquanto for igual ao lixo, vai puxando a palavra 
+                while (string[j+1] != '\0') { // Puxa a palavra para trás 
                     string[j] = string[j+1];
                         j++;
                 }
-                string[j] = string[j+1];
+                string[j] = string[j+1]; // Troca por causa do \0 
             }
         }
         else {
-            if (lixo == string[i]) {
+            if (lixo == string[i]) { // Para cobrir o caso ímpar
                 j = i;
                 while (string[j+1] != '\0') {
                     string[j] = string[j+1];
