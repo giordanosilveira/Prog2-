a64e97c56facafda6e8ca339880c1975ec94ba86
giordanosilveira <55298486+giordanosilveira@users.noreply.github.com>
2020-03-03 20:37:48 -0300

    Update ex_string3.c

--- Strings/3/ex_string3.c
+++ Strings/3/ex_string3.c
@@ -14,12 +14,12 @@ int main () {
     char string[MAX];
     int i;
 
-    scanf ("%[^\n]", string);
+    scanf ("%[^\n]", string); // Lê enquando não enxerga um espaço
     getchar ();
 
-    for (i = 0; i < strlen(string); i++) {
+    for (i = 0; i < strlen(string); i++) {  // Se o char for minúsculo, troca ele para maiúsculo
         if (string[i] > 96 && string[i] < 123)
-            string[i] = string[i] - 32;
+            string[i] = string[i] - 32; // Conta para pegar o char certo
     }
 
     printf ("%s\n", string);
