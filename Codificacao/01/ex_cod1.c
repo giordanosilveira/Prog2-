84a71f1e99713394c5720584d52ec5bc7332f9ee
Giba pingo <giordanovraau0204@gmail.com>
2020-03-06 20:22:57 -0300

    Ta indo

--- Codificacao/01/ex_cod1.c
+++ Codificacao/01/ex_cod1.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #define MAX 100
 
+int testacharmin (int i, unsigned char *word) {
+    if (word[i] < 224 || word[i] > 227)
+        return 1;
+    if (word[i] < 233 || word[i] > 234)
+        return 2;
+    if (word[i] < 237 || word[i] > 238)
+        return 3;
+    if (word[i] < 243 || word[i] > 245)
+        return 4;
+    if (word[i] < 250 || word[i] > 251)
+        return 5;
+    if (word[i] == 231)
+        return 6;
+
+    return 0;
+}
 int testacharmai (int i, unsigned char *word) {
 
     if (word[i] < 192 || word[i] > 195)
@@ -11,7 +27,7 @@ int testacharmai (int i, unsigned char *word) {
         return 2;
     if (word[i] < 205 || word[i] > 206)
         return 3;
-    if (word[i] < 211 || word[i] > 212)
+    if (word[i] < 211 || word[i] > 213)
         return 4;
     if (word[i] < 218 || word[i] > 219)
         return 5;
@@ -27,35 +43,8 @@ void turntoascii (unsigned char *word) {
     tam = strlen (word);
     i = 0;
     while (i < tam) {
-
-        switch (testachar(i,word))
-        {
-        case /* constant-expression */:
-            /* code */
-        break;
-    
-        case /* constant-expression */:
-            /* code */
-        break;
-    
-        case /* constant-expression */:
-            /* code */
-        break;
-    
-        case /* constant-expression */:
-            /* code */
-        break;
-    
-        case /* constant-expression */:
-        /* code */
-        break;
-    
-        case /* constant-expression */:
-            /* code */
-        break;
-    
-        default:
-        break;
+        if (testachar(i,word))
+            trocachar (i,word)
     }
 }
 int main () {
