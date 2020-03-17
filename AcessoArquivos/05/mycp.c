18d3c954d2422a22237480e11b7cebc072c74f91
Giordano Silveira <ghs19@inf.ufpr.br>
2020-03-17 12:55:17 -0300

    Rapa 0 e começa tudo de novo

--- AcessoArquivos/05/mycp.c
+++ AcessoArquivos/05/mycp.c
@@ -9,24 +9,24 @@ int main (int argc, char **argv) {
     char c;
 
     orig = fopen (argv[1], "r");
-    dest = fopen (argv[2], "w");
+    //t = fopen (argv[2], "w");
 
     if (! orig ) {
         perror ("Não foi possível abrir o arquivo");
         exit (1);
     }
 
-    if( access( argv[2], F_OK ) != -1 ) {
+    /*if( access( argv[2], F_OK ) != -1 ) {
         printf ("O arquivo já existe\n");
         exit (1);
-    }
+    }*/
 
     c = fgetc (orig); 
     while (c != EOF) {
-        fprintf (dest,"%c",c);
+        printf ("%c",c);
         c = fgetc (orig);
     }
-    fprintf (dest,"%c", '\n');
+    //fprintf (dest,"%c", '\n');
 
     return 0;
 }
