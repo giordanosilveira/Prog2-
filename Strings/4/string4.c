8c8e875d8731a0da8e352a53ab2fb5ced66c24d4
Giordano Silveira <giordanovraau0204@gmail.com>
2021-09-24 15:57:53 -0300

    tenho q ver o q está errado

--- Strings/4/string4.c
+++ Strings/4/string4.c
@@ -4,3 +4,120 @@
 -----------------------------------------------------------------------------------------------------------------------------------------------
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 6
+
+char * aloca_vetor (int n) {
+
+    char *vetor;
+
+    vetor = malloc (sizeof(char)*MAX);
+    if (! vetor) {
+        perror ("Operação inválida");
+        exit (1);
+    }
+
+   return vetor;
+
+}
+char **aloca_matriz (int n) {
+
+    char **matriz;
+
+    matriz = malloc (sizeof(char *)*MAX);
+    if (! matriz) {
+        perror ("Operação inválida");
+        exit (1);
+    }
+
+    for (int i = 0; i < MAX; i++) {
+        matriz[i] = malloc (sizeof(char)*MAX);
+        if (! matriz[i]) {
+            perror ("Operação inválida");
+            exit (1);
+        }
+    }
+   
+   return matriz;
+}
+
+void ordena_palavras (char** matriz, int tam) {
+   
+   int i, j, k, min_linha;
+   char *ptraux;
+
+   for (i = 0; i < tam; i++) {
+      j = 0;
+      min_linha = i;
+
+      for (k = i + 1; k < tam; k++ ) {
+         if (matriz[k][j] < matriz[min_linha][j] )
+              min_linha = k;
+
+         if (matriz[k][j] == matriz[min_linha][j] ) {
+            
+            while ((matriz[k][j] == matriz[min_linha][j]) && (matriz[k][j] != '\0') && (matriz [min_linha][j] != '\0'))
+               j++;
+
+            if (matriz[k][j] < matriz[min_linha][j])
+               min_linha = k;
+         }
+         
+         if ( min_linha != i) {
+
+             ptraux = matriz[i];
+             matriz[i] = matriz[min_linha];
+             matriz[min_linha] = ptraux;
+         }
+      }
+
+   }
+        
+}
+
+void escreve_palavras (char **matriz, int tam) {
+
+    int i;
+
+    for (i = 0; i < tam; i++) {
+        printf ("%s\n", matriz[i]);
+    }
+}
+
+void libera_alocacao (char **matriz, char *vetor, int tam ) {
+
+    free (vetor);
+    for (int i = 0; i < tam; i++)
+        free (matriz[i]);
+
+   free (matriz);
+}
+
+int main () {
+
+   int i;
+   char *linha, **palavras;
+   
+   i = 0;
+   linha = aloca_vetor (MAX);
+   palavras = aloca_matriz (MAX);
+
+   fgets (linha, MAX, stdin);
+   while ( (i < MAX) && ((strcmp (linha, "x")) != 0)){
+      strcpy (palavras[i], linha);
+      fgets (linha, MAX, stdin);
+      i++;
+   } 
+
+   ordena_palavras (palavras, i);
+
+   escreve_palavras (palavras, i);
+
+   libera_alocacao (palavras, linha, i);
+
+   return 0;
+}
+
+
